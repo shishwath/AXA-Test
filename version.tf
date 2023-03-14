937f03dbe4fcaeb134dffa84d144b547ef9cf0b0
shishwath <69855073+shishwath@users.noreply.github.com>
2023-03-14 17:42:54 +0530

    Update version.tf

--- version.tf
+++ version.tf
@@ -1,6 +1,13 @@
-provider "azurerm" {
-    version = "~>2.0"
-    features {
-      
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
     }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
 }
